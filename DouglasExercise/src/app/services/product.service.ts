5b4e0516b25c04ac29547c213620db40081315d6
Mujamil <mujamil@mac-users-MacBook-Pro.local>
2018-12-03 13:29:40 +0100

    updated product with proper description

--- DouglasExercise/src/app/services/product.service.ts
+++ DouglasExercise/src/app/services/product.service.ts
@@ -10,21 +10,17 @@ export class ProductService {
         {
           name : 'Banana',
           type : 'fruit',
-          description: `banana fruit description banana
-          fruit descriptionbanana
-          fruit descriptionbanana
-           fruit descriptionbanana
-           fruit descriptionbanana
-           fruit descriptionbanana
-           fruit descriptionbanana
-           fruit descriptionbanana fruit descriptionbanana fruit descriptionbanana
-           fruit descriptionbanana fruit descriptionbanana fruit descriptionbanana fruit
-           descriptionbanana fruit description`
+          description: `A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering
+plants in the genus Musa. In some countries, bananas used for cooking may be called "plantains",
+distinguishing them from dessert bananas.`
         },
         {
           name : 'Chicken',
           type : 'meat',
-          description: 'banana meat description'
+          description: `Chickens are gregarious birds and live together in flocks. They have a communal approach to the
+incubation of eggs and raising of young. Individual chickens in a flock will dominate others, establishing a "pecking order",
+with dominant individuals having priority for food access and nesting locations.
+This behaviour may also be observed in mother hens to call their chicks and encourage them to eat.`
         },
         {
           name : 'Broccoli',
